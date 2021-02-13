978147c755941fdb66c441526aff965499a4a3a7
colby <colby@thebuzzframe.com>
2021-02-13 09:52:45 -0500

    add in todays counter:

--- src/App.js
+++ src/App.js
@@ -6,7 +6,35 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    let today=new Date();
+    let todayStr= "" + today.getFullYear() + today.getMonth() + today.getDate();
+
+    var currentCount = Number.parseInt(localStorage.getItem(todayStr));
+
+    currentCount = !currentCount ? 0 : currentCount;
+
+    this.state = {
+      todaysCount: currentCount,
+      todayStr: todayStr
+    };
+
+
+  }
+
+  changeValue(number) 
+  {
+    let newCount = this.state.todaysCount + number;
+
+    if(newCount <= 0)
+    {
+      newCount = 0;
+    }
+    return () => {
+      localStorage.setItem(this.state.todayStr, newCount);
+      this.setState({
+        todaysCount: newCount
+      });
+    };
   }
 
   render() {
@@ -29,8 +57,14 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <div className="item-container">
-            <div className="title">Today's Number</div>
-            <div className="value">{days}</div>
+            <div className="title">Today's Goal: {days}</div>
+          </div>
+          <div className="todays-count-container">
+            <div className="change-value" onClick={this.changeValue(-5)}>-5</div>
+            <div className="change-value" onClick={this.changeValue(-1)}>-1</div>
+            <div className="todays-count">{this.state.todaysCount}</div>
+            <div className="change-value" onClick={this.changeValue(1)}>+1</div>
+            <div className="change-value" onClick={this.changeValue(5)}>+5</div>
           </div>
           <div className="item-container">
             <div className="title">Total Completed</div>
