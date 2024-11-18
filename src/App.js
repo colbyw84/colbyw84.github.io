fb22e69604789c0990c6419d1d891c3da38904b2
colby <colby@thebuzzframe.com>
2024-11-18 07:13:48 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import DailyCounter from './components/daily-counter/daily-counter';
 import DailyTasks from './components/daily-tasks/daily-tasks';
 import NewYearChallenge from './components/new-year-challenge/new-year-challenge';
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  var params = new URLSearchParams(useLocation().search);
+  const [page, setPage] = useState(params.get('page')); // Initializing state if needed.
 
-    this.state = {};
-  }
-
-  responseGoogle(response) {
+  const responseGoogle = (response) => {
     console.log(response);
-  }
+  };
 
-  getTab() {
-    return 
-  }
+  const getTab = () => {
+    if(page === 'workout')
+    {
+      return <NewYearChallenge />
+    }
+    else if(page === 'tasks')
+      {
+        return <DailyTasks />
+      }
+    else if(page === 'counter')
+      {
+        return <DailyCounter />
+      }
+    else {
+      return <DailyTasks />;
+    }
+  };
 
-  render() {
-
-    return (
-      <div className="App">
-        <div  className="container">
-          <Routes>
-            <Route path="?page=workout" element={<NewYearChallenge />} />
-            <Route path="?page=daily-tasks" element={<DailyTasks />} />
-            <Route path="?page=daily-counter" element={<DailyCounter />} />
-            <Route path="*" element={<DailyTasks />} />
-          </Routes>
-        </div>
+  return (
+    <div className="App">
+      <div className="container">
+        {getTab()}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
